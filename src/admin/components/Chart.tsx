35cb7c431c357ff6fd6480a86eeb9c35e2bf84b0
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-05-06 17:40:31 -0300

    update shadcn class name

--- src/admin/components/Chart.tsx
+++ src/admin/components/Chart.tsx
@@ -19,7 +19,7 @@ const Chart: React.FC<ChartProps> = ({ title, data }) => {
             </div>
             <div className="flex-1 bg-gray-200 rounded-full h-3">
               <div 
-                className="bg-gradient-to-r from-blue-500 to-blue-600 h-3 rounded-full transition-all duration-1000 ease-out"
+                className="linear-gradient-to-r from-blue-500 to-blue-600 h-3 rounded-full transition-all duration-1000 ease-out"
                 style={{ width: `${(item.value / maxValue) * 100}%` }}
               ></div>
             </div>
