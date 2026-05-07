c6cb629db345d69ee2efd5e9de73aba935520e22
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-05-07 15:39:40 -0300

    update import

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { TesloShopApp } from './TesloShopApp'
+import { LoreVaultApp } from './LoreVaultApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TesloShopApp />
+    <LoreVaultApp />
   </StrictMode>,
 )
