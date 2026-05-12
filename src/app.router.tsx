883bda6a4c5d3c28e7bdd0088de8032a17925fb9
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-05-12 20:44:40 -0300

    add new routes

--- src/app.router.tsx
+++ src/app.router.tsx
@@ -1,23 +1,24 @@
 /* eslint-disable react-refresh/only-export-components */
 import { lazy } from "react";
-import { createHashRouter, Navigate } from "react-router";
+import { createBrowserRouter, /* createHashRouter, */ Navigate } from "react-router";
 
 import { ShopLayout } from "./shop/layouts/ShopLayout";
 import { HomePage } from "./shop/pages/home/HomePage";
 import { ProductPage } from "./shop/pages/product/ProductPage"
-import { GenderPage } from "./shop/pages/gender/GenderPage";
 import { RegisterPage } from "./auth/pages/register/RegisterPage";
 import { LoginPage } from "./auth/pages/login/LoginPage";
 import { DashboardPage } from "./admin/pages/dashboard/DashboardPage";
 import { AdminProductsPage } from "./admin/pages/products/AdminProductsPage";
 import { AdminProductPage } from "./admin/pages/product/AdminProductPage";
 import { AdminRoute, NotAuthenticatedRoute } from "./components/routes/ProtectedRoutes";
+import { CategoriesPage } from "./admin/pages/categories/CategoriesPage";
+import { CheckoutPage } from "./admin/pages/checkout/CheckoutPage";
 
 const AuthLayout = lazy(() => import('./auth/layouts/AuthLayout'));
 const AdminLayout = lazy(() => import("./admin/layouts/AdminLayout"));
 
-export const appRouter = createHashRouter([
-// export const appRouter = createBrowserRouter([
+// export const appRouter = createHashRouter([
+export const appRouter = createBrowserRouter([
   // Main routes
   {
     path: '/',
@@ -28,12 +29,12 @@ export const appRouter = createHashRouter([
         element: <HomePage />
       },
       {
-        path: 'product/:idSlulg',
+        path: 'product/:id',
         element: <ProductPage />
       },
       {
-        path: 'gender/:gender',
-        element: <GenderPage />
+        path: 'checkout',
+        element: <CheckoutPage />
       }
     ]
   },
@@ -80,6 +81,10 @@ export const appRouter = createHashRouter([
       {
         path: 'products/:id',
         element: <AdminProductPage />
+      },
+      {
+        path: 'categories',
+        element: <CategoriesPage />
       }
     ]
   },
