f37e53d9e79bffbe72ee8a8a6d295bc4c7683048
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-05-15 10:37:52 -0300

    remove new empty product creation from action

--- src/actions/products/get-product-by-id.action.ts
+++ src/actions/products/get-product-by-id.action.ts
@@ -6,21 +6,6 @@ export const getProductByIdAction = async (id: string): Promise<ProductUI> => {
 
   if (!id) throw new Error('id is required');
 
-  if (id === 'new') {
-    return {
-      id: null,
-      title: '',
-      price: 0,
-      description: '',
-      slug: '',
-      stock: 0,
-      sizes: [],
-      gender: 'men',
-      tags: [],
-      images: []
-    } as unknown as ProductUI;
-  }
-
   const { data } = await backendApi.get<Product>(`/products/${id}`);
   
   return adaptProduct(data);
