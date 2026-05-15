8f82949d45447cf7f27b83dbc21827737c83c2e1
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-05-15 10:40:04 -0300

    refactor to not send empty product object when in create mode

--- src/admin/pages/product/AdminProductPage.tsx
+++ src/admin/pages/product/AdminProductPage.tsx
@@ -1,17 +1,16 @@
-// https://github.com/Klerith/bolt-product-editor
-
 import { Navigate, useParams } from 'react-router';
 
 import { useProduct } from '@/admin/hooks/useProduct';
 import { CustomLoading } from '@/components/custom/CustomLoading';
-import { ProductForm } from './ui/ProductForm';
+import { ProductForm } from './form/ProductForm';
 
 export const AdminProductPage = () => {
   const { id } = useParams();
+  const isNew = id === 'new';
 
-  const title = id === 'new' ? 'Add product' : 'Edit product';
+  const title = isNew ? 'Add product' : 'Edit product';
   const subtitle =
-    id === 'new'
+    isNew
       ? 'Add a new product to your inventory.'
       : 'Edit the details of your product and save the changes.';
 
@@ -25,16 +24,26 @@ export const AdminProductPage = () => {
 
   if (isError) return <Navigate to={'/admin/products'} />;
 
+  if (isNew) {
+    return <ProductForm
+      mode="create"
+      title={title}
+      subtitle={subtitle}
+      isPending={isPending}
+      onSubmit={handleSubmit}
+    />
+  }
+
   if (isLoading) return <CustomLoading />;
 
   if (!product) return <Navigate to={'/admin/products'} />;
 
   return <ProductForm
+    mode="edit"
     title={title}
     subtitle={subtitle}
     product={product}
     isPending={isPending}
     onSubmit={handleSubmit}
   />
-  
 };
