6977069678123f8bfa4678c6992d8699e23a12be
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-05-16 22:19:55 -0300

    add prop to check if should show, when collapsed on desktop and when in mobile

--- src/components/custom/CustomLogo.tsx
+++ src/components/custom/CustomLogo.tsx
@@ -1,22 +1,25 @@
-import { Link } from "react-router"
+import { Link } from "react-router";
 
 interface Props {
   to?: string;
   subtitle?: string;
+  shouldShow?: boolean;
 }
 
-export const CustomLogo = ({ to = '/', subtitle = 'Market' }: Props) => {
+export const CustomLogo = ({ to = '/', subtitle = 'Market', shouldShow }: Props) => {
   return (
-    <Link to={to} className="flex items-center gap-2 shrink-0">
-      <img src="/logo.png" alt="logo" className="h-10 w-10" />
-      <div className="hidden sm:block">
-        <h1 className="text-lg font-semibold tracking-tight text-foreground">
-          The LoreVault
-        </h1>
-        <p className="text-[10px] uppercase tracking-widest text-muted-foreground -mt-1 font-cinzel">
-          {subtitle}
-        </p>
-      </div>
-    </Link>
+    shouldShow && (
+      <Link to={to} className="flex items-center gap-2 shrink-0">
+        <img src="/logo.png" alt="logo" className="h-10 w-10" />
+        <div>
+          <h1 className="text-lg font-semibold tracking-tight text-foreground">
+            The LoreVault
+          </h1>
+          <p className="text-[10px] uppercase tracking-widest text-muted-foreground -mt-1 font-cinzel">
+            {subtitle}
+          </p>
+        </div>
+      </Link>
+    )
   )
 }
