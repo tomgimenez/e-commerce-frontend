c34728c8ca262c3bebed72283a9882e9a9910eff
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-05-16 22:17:06 -0300

    update import

--- src/admin/hooks/useCategories.tsx
+++ src/admin/hooks/useCategories.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getCategoriesAction } from "../actions/get-categories";
+import { getCategoriesAction } from "../actions/get-categories.action";
 
 export const useCategories = () => {
   return useQuery({
