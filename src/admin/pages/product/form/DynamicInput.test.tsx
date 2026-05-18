21461ae938cd0d6e7e0a3e2e506668b76c9b24aa
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-05-18 20:40:08 -0300

    complete test

--- src/admin/pages/product/form/DynamicInput.test.tsx
+++ src/admin/pages/product/form/DynamicInput.test.tsx
@@ -1,7 +1,34 @@
-import { describe, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
+import { DynamicInput } from "./DynamicInput";
+
+vi.mock('react-hook-form', () => ({
+  register: vi.fn()
+}))
+
+const mockedField = {
+  fieldKey: 1,
+  field: {
+    type: 'text',
+    required: true,
+    label: 'Mocked Field'
+  },
+  register: () => {},
+  errors: { attributes: null}
+} as unknown as React.ComponentProps<typeof DynamicInput>;
 
 describe('DynamicInput', () => {
   test('should render correctly', ()=> {
+    render(
+      <DynamicInput
+        fieldKey={mockedField.fieldKey}
+        field={mockedField.field}
+        register={mockedField.register}
+        errors={mockedField.errors}
+      />
+    );
 
+    expect(screen.getByText('Mocked Field')).toBeInTheDocument()
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
   });
 })
