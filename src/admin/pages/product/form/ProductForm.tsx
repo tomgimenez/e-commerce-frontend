44ca5f8717945b16e6cea2be65c607412018c139
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-05-21 17:20:27 -0300

    add type to button

--- src/admin/pages/product/form/ProductForm.tsx
+++ src/admin/pages/product/form/ProductForm.tsx
@@ -46,7 +46,7 @@ export const ProductForm = (props: Props) => {
         <div className="flex justify-between items-center">
           <AdminTitle title={title} subtitle={subtitle} />
           <div className="flex justify-end mb-10 gap-4">
-            <Button variant="outline">
+            <Button variant="outline" type="button">
               <Link to="/admin/products" className="flex items-center gap-2">
                 <X className="w-4 h-4" />
                 Cancel
