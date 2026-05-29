83f282fc12f8351e8f098f0bbaa08479c628cb18
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-05-29 17:39:16 -0300

    add config to ignore 'any' type errors in tests

--- eslint.config.js
+++ eslint.config.js
@@ -19,4 +19,10 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
+  {
+    files: ['**/*.test.ts', '**/*.test.tsx', '**/*.spec.ts', '**/*.spec.tsx'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  }
 ])
