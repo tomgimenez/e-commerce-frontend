2e46c7f8a962ddaf4f908bffa592925c3d089a2a
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-06-14 17:13:20 -0300

    add cart entities interface

---
+++ src/interfaces/cart.interface.ts
@@ -0,0 +1,20 @@
+import type { Product } from "@/interfaces/product.interface";
+
+export interface Cart {
+  id:        string;
+  updatedAt: Date;
+  items:     CartItem[];
+}
+
+export interface CartItem {
+  id:        string;
+  quantity:  number;
+  unitPrice: number;
+  product:   Product;
+}
+
+export interface CartItemDto {
+  productId: string;
+  unitPrice: number;
+  quantity: number;
+}
