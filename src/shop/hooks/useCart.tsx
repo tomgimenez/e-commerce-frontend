efda2d27a434746f339807974a70a3ab89a3b507
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-06-16 20:25:53 -0300

    add functionality to let guest users save items in session cart

--- src/shop/hooks/useCart.tsx
+++ src/shop/hooks/useCart.tsx
@@ -1,17 +1,54 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { addItem, getCart, removeItem, updateItem } from "../api/cart.api"
 import type { CartItem, CartItemDto } from "@/interfaces/cart.interface";
+import { useCartStore } from "../store/cart.store";
+import { useAuthStore } from "@/auth/store/auth.store";
 
 export const useCart = () => {
 
   const queryClient = useQueryClient();
+  const { guestItems, addGuestItem, updateGuestItem, removeGuestItem } = useCartStore();
+  const { authStatus } = useAuthStore();
+
+  const isLoggedIn = authStatus === 'authenticated';
   
-  const { data: cart } = useQuery({
+  const { data: userCart } = useQuery({
     queryKey: ['cart'],
     queryFn: () => getCart(),
     retry: false,
+    enabled: isLoggedIn,
     staleTime: 1000 * 60 * 5
   });
+
+  const cart = isLoggedIn ? userCart : { items: guestItems };
+
+  const handleAddItem = (item: CartItem, onSuccess?: () => void) => {
+    if (isLoggedIn) {
+      addItemMutation.mutate({
+        productId: item.product.id,
+        unitPrice: item.unitPrice,
+        quantity: item.quantity
+      }, { onSuccess })
+    } else {
+      addGuestItem(item);
+      onSuccess?.();
+    }
+  }
+
+  const handleUpdateItem = (item: CartItem) => {
+    if (isLoggedIn)
+      updateItemMutation.mutate(item);
+    else
+      updateGuestItem(item.product.id, item.quantity);
+  }
+
+  const handleRemoveItem = (item: CartItem) => {
+    if (isLoggedIn) {
+      removeItemMutation.mutate(item.id!)
+    } else {
+      removeGuestItem(item.product.id)
+    }
+  }
   
   const addItemMutation = useMutation({
     mutationFn: (item: CartItemDto) => addItem(item),
@@ -31,14 +68,14 @@ export const useCart = () => {
     mutationFn: (cartItemId: string) => removeItem(cartItemId),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['cart'] });
-    },  
+    }, 
   });  
 
   return { 
     cart,
-    addItem: (item: CartItemDto, onSuccess?: () => void) => addItemMutation.mutate(item, { onSuccess }),
-    removeItem: removeItemMutation.mutate,
-    updateItem: updateItemMutation.mutate
+    addItem: handleAddItem,
+    removeItem: handleRemoveItem,
+    updateItem: handleUpdateItem
   }
 }
 
--- src/shop/store/cart.store.ts
+++ src/shop/store/cart.store.ts
@@ -1,15 +1,51 @@
+import type { CartItem } from "@/interfaces/cart.interface";
 import { create } from "zustand";
+import { persist } from "zustand/middleware"
 
 interface CartStore {
+  // drawer
   lastAdded: { title: string; } | null;
   isDrawerOpen: boolean;
   openDrawer: (item: { title: string; }) => void;
   closeDrawer: () => void;
+
+  // guest cart
+  guestItems: CartItem[];
+  addGuestItem: (item: CartItem) => void;
+  updateGuestItem: (productId: string, quantity: number) => void;
+  removeGuestItem: (productId: string) => void;
 }
 
-export const useCartStore = create<CartStore>((set) => ({
-  lastAdded: null,
-  isDrawerOpen: false,
-  openDrawer: (item) => set({ isDrawerOpen: true, lastAdded: item }),
-  closeDrawer: () => set({ isDrawerOpen: false })
-}));
+export const useCartStore = create<CartStore>()(
+  persist(
+    (set, get) => ({
+      lastAdded: null,
+      isDrawerOpen: false,
+      openDrawer: (item) => set({ isDrawerOpen: true, lastAdded: item }),
+      closeDrawer: () => set({ isDrawerOpen: false }),
+
+      // Guest Cart
+      guestItems: [],
+      addGuestItem: (item) => {
+        const existing = get().guestItems.find(i => i.product.id === item.product.id);
+        if (existing) {
+          set({ guestItems: get().guestItems.map(i =>
+            i.product.id === item.product.id
+              ? {...i, quantity: i.quantity + item.quantity}
+              : i 
+          )})
+        } else {
+          set({ guestItems: [...get().guestItems, {...item, id: crypto.randomUUID()}] })
+        }
+      },
+      updateGuestItem: (productId, quantity) => 
+        set({ guestItems: get().guestItems.map( i =>
+          i.product.id === productId ? { ...i, quantity } : i
+        )}),
+      removeGuestItem: (productId: string) => set({ guestItems: get().guestItems.filter(i => i.product.id !== productId) })
+    }),
+  {
+    name: "guest-cart",
+    partialize: (state) => ({ guestItems: state.guestItems })
+  })
+);
