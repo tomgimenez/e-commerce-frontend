b546a498d72a2a034dded9450a93c0400a6f3966
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-06-19 17:47:55 -0300

    add address interface and custom hook

---
+++ src/interfaces/address.interface.ts
@@ -0,0 +1,11 @@
+export interface Address {
+  id: string;
+  name: string;
+  street: string;
+  number: string;
+  city: string;
+  state: string;
+  zip_code: string;
+  country: string;
+  is_default: boolean;
+}
