4be6beb419359223382e89a368242fd59c362dd2
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-07-23 17:34:22 -0300

    add correct styles to layout

--- src/admin/layouts/AdminLayout.tsx
+++ src/admin/layouts/AdminLayout.tsx
@@ -7,13 +7,13 @@ import { AdminHeader } from "../components/AdminHeader";
 const AdminLayout = () => {
 
   return (
-    <div className="min-h-screen flex">
+    <div className="min-h-screen bg-background">
       <AdminSidebar />
       
-      <div className="flex-1 flex flex-col">
+      <div className="lg:pl-64">
         <AdminHeader />
         
-        <main className="flex-1 p-6">
+        <main className="p-6">
           <Outlet />
         </main>
       </div>
